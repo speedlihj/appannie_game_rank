f8cb6447205fae0caf2d5930a658253980946119
李洪基 <lihongji@yodo1.com>
2017-06-28 11:26:03 +0800

    使用OrderedDict 按指定顺序输出每行

--- appannie_game_rank/pipelines.py
+++ appannie_game_rank/pipelines.py
@@ -7,12 +7,20 @@
 
 import json
 import codecs
+import collections
 
 class AppannieGameRankPipeline(object):
     def __init__(self):
         self.file = codecs.open('items.json', 'wb', encoding='utf-8')
 
     def process_item(self, item, spider):
-        line = json.dumps(item) + ",\n"
+        d1 = collections.OrderedDict()
+        d1['date']=item['date']
+        d1['rank'] = item['rank']
+        d1['free_rank_name']=item['free_rank_name']
+        d1['paid_rank_name']=item['paid_rank_name']
+        d1['sale_rank_name']=item['sale_rank_name']
+
+        line = json.dumps(d1) + ",\n"
         self.file.write(line.decode('unicode_escape'))
         return item
